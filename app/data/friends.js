61e047490cdc541100c5c4ceb0221fa4e9f34d19
eeprom01 <sean.ton@gmail.com>
2017-10-22 16:07:13 -0600

    first commit

---
+++ app/data/friends.js
@@ -0,0 +1,86 @@
+var potential = {
+  "name":"Jim",
+  "photo":"/../public/images/Jim.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}   
+
+{
+  "name":"Julian",
+  "photo":"/../public/images/Julian.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}   
+
+{
+  "name":"Ricky",
+  "photo":"/../public/images/ricky.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}   
+
+{
+  "name":"Bubbles",
+  "photo":"/../public/images/trailer_park_boys_1.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}   
+
+{
+  "name":"Randy",
+  "photo":"/../public/images/Randy.jpg",
+  "scores":[
+      5,
+      1,
+      4,
+      4,
+      5,
+      1,
+      2,
+      5,
+      4,
+      1
+    ]
+}   
+
+module.exports = potential;
