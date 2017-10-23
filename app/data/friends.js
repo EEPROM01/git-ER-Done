7d1167bccf4a1763243f781f3ec2a84357fc1035
eeprom01 <sean.ton@gmail.com>
2017-10-22 18:03:26 -0600

    fresh

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,4 +1,5 @@
-var potential = {
+var potential = [
+  {
   "name":"Jim",
   "photo":"/../public/images/Jim.jpg",
   "scores":[
@@ -13,8 +14,7 @@ var potential = {
       4,
       1
     ]
-}   
-
+},  
 {
   "name":"Julian",
   "photo":"/../public/images/Julian.jpg",
@@ -30,8 +30,7 @@ var potential = {
       4,
       1
     ]
-}   
-
+},
 {
   "name":"Ricky",
   "photo":"/../public/images/ricky.jpg",
@@ -47,40 +46,39 @@ var potential = {
       4,
       1
     ]
-}   
-
+},   
 {
   "name":"Bubbles",
   "photo":"/../public/images/trailer_park_boys_1.jpg",
   "scores":[
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
+    5,
+    1,
+    4,
+    4,
+    5,
+    1,
+    2,
+    5,
+    4,
+    1
     ]
-}   
-
+},   
 {
   "name":"Randy",
   "photo":"/../public/images/Randy.jpg",
   "scores":[
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
+    5,
+    1,
+    4,
+    4,
+    5,
+    1,
+    2,
+    5,
+    4,
+    1
     ]
 }   
+]
 
 module.exports = potential;
--- server.js
+++ server.js
@@ -5,22 +5,21 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
-// ==============================================================================
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server
-// ==============================================================================
+
 // Tells node that we are creating an "express" server
 var app = express();
-// Sets an initial port. We"ll use this later in our listener
+
 var PORT = process.env.PORT || 8080;
+
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// Points our server to a ser ies of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+app.use(bodyParser.text());
+app.use(bodyParser.json({type:'application/vnd.api+json'}));
+// Points server to  files route(s)
 // <><><><<>><><><><><><><><><><><><><>
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 
 //<><><>><><><><><><><><><><><><><>><><><>
